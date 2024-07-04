0664f77739b620eac7b3411319491d6b8a9594be
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-07-04 21:38:04 +0600

    approximatly done

--- app.js
+++ app.js
@@ -1,1 +1,145 @@
-console.log('paisi ore')
+let images_dt = [
+    {
+        title : "THE CHICKEN <br> BURGER",
+        price : "$12.90",
+        MRP : "$24",
+        calorie : 7845,
+        fat : 98 ,
+        protein : 112
+    },
+    {
+        title : "THE PINK <br> BURGER",
+        price : "$18.90",
+        MRP : "$38",
+        calorie : 4512,
+        fat : 65,
+        protein : 78
+    },
+    {
+        title : "THE BLACK <br> BURGER",
+        price : "$10.90",
+        MRP : "$18",
+        calorie : 6545,
+        fat : 115 ,
+        protein : 56
+    },
+    {
+        title : "THE RED <br> BURGER",
+        price : "$11.90",
+        MRP : "$20",
+        calorie : 9845,
+        fat : 68 ,
+        protein : 86
+    },
+    {
+        title : "THE GREEN <br> BURGER",
+        price : "$13.90",
+        MRP : "$26",
+        calorie : 1256,
+        fat : 45 ,
+        protein : 123
+    },
+    {
+        title : "THE TORNADO <br> BURGER",
+        price : "$18.90",
+        MRP : "$38",
+        calorie : 5678,
+        fat : 92 ,
+        protein : 99
+    },
+    {
+        title : "THE COMBO <br> BURGER",
+        price : "$9.90",
+        MRP : "$17",
+        calorie : 3245,
+        fat : 49,
+        protein : 78
+    },
+]
+
+let index = 0;
+let len = (document.getElementsByClassName('main_imgs')).length*230
+
+let images_off=() =>{
+
+    Array.from(document.getElementsByClassName('main_imgs')).forEach((el) =>{
+            el.style= "width:35%";
+            el.style.filter= "blur(1px)";
+
+    })
+}
+let icon_off=() =>{
+
+    Array.from(document.getElementsByClassName('pagi_img')).forEach((el) =>{
+            el.style.transform= "unset";
+            el.style.opacity= ".3";
+
+    })
+}
+document.getElementsByClassName('bi bi-chevron-right')[0].addEventListener('click',() =>{
+    index +=230;
+    if(index >(len)-230){
+        index=(len)-230;
+
+
+    }
+    document.getElementsByClassName('images')[0].scrollLeft=index;
+    images_off();
+   document.getElementsByClassName('main_imgs')[index/230].style= "width:60%";
+   document.getElementsByClassName('main_imgs')[index/230].style.filter= "blur(0px)";
+   document.getElementsByClassName('title')[0].innerHTML=images_dt[index/230].title;
+  document.getElementsByClassName('price')[0].innerHTML = `${images_dt[index / 230].price} <sup><del>${images_dt[index / 230].MRP}</del></sup>`;
+  document.getElementById('calorie').innerText = images_dt[(index/230)].calorie;
+  document.getElementById('fat').innerText = images_dt[(index/230)].fat;
+  document.getElementById('protien').innerText = images_dt[(index/230)].protein;
+  icon_off();
+  (document.getElementsByClassName('pagi_img'))[index/230].style.transform ="scale(1.1)"
+  (document.getElementsByClassName('pagi_img'))[index/230].style.opacity = 1;
+
+
+
+})
+document.getElementsByClassName('bi bi-chevron-left')[0].addEventListener('click',() =>{
+    index -=230;
+    if(index< 0){
+        index=0;
+
+
+    }
+    document.getElementsByClassName('images')[0].scrollLeft=index;
+    images_off();
+   document.getElementsByClassName('main_imgs')[index/230].style= "width:60%";
+   document.getElementsByClassName('main_imgs')[index/230].style.filter= "blur(0px)";
+   document.getElementsByClassName('title')[0].innerHTML=images_dt[index/230].title;
+  document.getElementsByClassName('price')[0].innerHTML = `${images_dt[index / 230].price} <sup><del>${images_dt[index / 230].MRP}</del></sup>`;
+  document.getElementById('calorie').innerText = images_dt[(index/230)].calorie;
+  document.getElementById('fat').innerText = images_dt[(index/230)].fat;
+  document.getElementById('protien').innerText = images_dt[(index/230)].protein;
+  icon_off();
+  (document.getElementsByClassName('pagi_img'))[index/230].style.transform ="scale(1.1)"
+  (document.getElementsByClassName('pagi_img'))[index/230].style.opacity = 1;
+
+
+
+})
+
+Array.from(document.getElementsByClassName('pagi_img')).forEach((el,i)=>{el.addEventListener('click',()=>{
+
+        index=230*i;
+
+     document.getElementsByClassName('images')[0].scrollLeft=index;
+    images_off();
+   document.getElementsByClassName('main_imgs')[index/230].style= "width:60%";
+   document.getElementsByClassName('main_imgs')[index/230].style.filter= "blur(0px)";
+   document.getElementsByClassName('title')[0].innerHTML=images_dt[index/230].title;
+  document.getElementsByClassName('price')[0].innerHTML = `${images_dt[index / 230].price} <sup><del>${images_dt[index / 230].MRP}</del></sup>`;
+  document.getElementById('calorie').innerText = images_dt[(index/230)].calorie;
+  document.getElementById('fat').innerText = images_dt[(index/230)].fat;
+  document.getElementById('protien').innerText = images_dt[(index/230)].protein;
+  icon_off();
+  (document.getElementsByClassName('pagi_img'))[index/230].style.transform ="scale(1.1)"
+  (document.getElementsByClassName('pagi_img'))[index/230].style.opacity = 1;
+
+})
+
+})
